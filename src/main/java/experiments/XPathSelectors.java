6c50035a2f722fa0fcd522b86728228a3131be6c
tdshe <tnshvts1602@gmail.com>
2024-11-25 14:24:29 +0200

    Created xPathSel

---
+++ src/main/java/experiments/XPathSelectors.java
@@ -0,0 +1,98 @@
+package experiments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class XPathSelectors
+{
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void selectorRadioButtonPage()
+    {
+        driver.get("https://demoqa.com/radio-button");
+        driver.manage().window().maximize();
+        hideBanner();
+        hideFooter();
+
+        WebElement rbtnImp = driver.findElement(By.xpath("//label[@for='impressiveRadio']"));
+        rbtnImp.click();
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+    }
+
+    @Test
+    public void selectorTextBox()
+    {
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        hideBanner();
+        hideFooter();
+
+        WebElement textB = driver.findElement(By.xpath("//input[@placeholder='Full Name']"));
+        textB.sendKeys("Tanya");
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        WebElement email = driver.findElement(By.xpath("//form/div[@id='userEmail-wrapper']//input"));
+        email.sendKeys("tns@gmail.com");
+
+        WebElement text = driver.findElement(By.xpath("//*[text()='Current Address']"));
+        System.out.println(text.getText());
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        WebElement field = driver.findElement(By.xpath("//*[text()='Current Address']/../..//textarea"));
+        field.sendKeys("kdkskwklwllallsll");
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+    }
+
+    private void hideBanner()
+    {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+
+    private void hideFooter()
+    {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
+    }
+}
